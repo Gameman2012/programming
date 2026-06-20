071293128cd4d023a7ce0207ece1b8b759d99b58
Gameman2012 <mahmoudashrafibrahim1@gmail.com>
2026-06-20 12:35:26 +0300

    Auto-commit

--- shell/start-sys/start.sh
+++ shell/start-sys/start.sh
@@ -10,5 +10,5 @@ wpctl set-volume 53 0.50
 /home/mahmoud/programming/shell/start-sys/backup/backup-settings.sh &
 #start sound
 if test (brightnessctl -m | cut -d, -f4 | tr -d '%') -eq 100 -a (pactl get-sink-volume @DEFAULT_SINK@ | head -n1 | awk '{print $5}' | tr -d '%') -ge 30
-        mpv --no-video /home/mahmoud/programming/shell/start-sys/Windows XP Startup.mp3  &
+        mpv --no-video /home/mahmoud/programming/shell/start-sys/Windows\ XP\ Startup.mp3  &
 end
